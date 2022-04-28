b12b815ada08033a45bd459c76f30b1b8490986a
Bill Wallace <wayfarer3130@gmail.com>
2022-04-28 16:31:36 -0400

    Fix OHIFv2 refresh

--- packages/static-wado-webserver/lib/routes/client/viewer.mjs
+++ packages/static-wado-webserver/lib/routes/client/viewer.mjs
@@ -2,6 +2,6 @@ import { htmlMap } from "../../adapters/requestAdapters.mjs";
 import { defaultGetStaticController as staticController } from "../../controllers/client/staticControllers.mjs";
 
 export default function setViewerRoutes(routerExpress, params, dir) {
-  routerExpress.get("/viewer", htmlMap);
+  routerExpress.get(["/viewer", "/viewer/*"], htmlMap);
   routerExpress.use(staticController(dir));
 }
