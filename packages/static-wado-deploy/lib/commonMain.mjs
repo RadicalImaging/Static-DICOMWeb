0a62d91259f12f1799ee466740e6333e48ee497c
Bill Wallace <wayfarer3130@gmail.com>
2023-03-31 11:54:11 -0400

    fix: Add some error checks

--- packages/static-wado-deploy/lib/commonMain.mjs
+++ packages/static-wado-deploy/lib/commonMain.mjs
@@ -1,17 +1,14 @@
-export default function commonMain(config, name, options, storeF) {
+export default function commonMain(config, name, options, deploymentFunction) {
   const deployPlugin = config.deployPlugin;
   console.log(`Deploy ${name}`, deployPlugin);
   const deployments = config.deployments;
   if (deployments) {
-    return Promise.all(
-      deployments.map((deployment) => {
-        if (deployment[`${name}Group`] && (!options.deployments || options.deployments.includes(deployment.name))) {
-          return storeF(deployment, name, options, deployPlugin);
-        }
-        console.log("skipping deployment", deployment.name);
-        return null;
-      })
-    );
+    const deployment = deployments.find(deployment => deployment[`${name}Group`] && options.deployments.includes(deployment.name));
+    if ( !deployment ) {
+      console.warn("Deployment", options.deployments, "not found containing", name, "Group");
+      return -1;
+    }
+    return deploymentFunction(deployment, name, options, deployPlugin);
   }
-  return storeF(config, name, options, deployPlugin);
+  return deploymentFunction(config, name, options, deployPlugin);
 }
