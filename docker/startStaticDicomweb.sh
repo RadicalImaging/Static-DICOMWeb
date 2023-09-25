f6f9f4d5d50df1cb497f62b7d890953132f796a3
Bill Wallace <wayfarer3130@gmail.com>
2023-09-25 11:47:25 -0400

    Minimize the run script

--- docker/startStaticDicomweb.sh
+++ docker/startStaticDicomweb.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-echo Hello start static dicomweb
 cd /app
-cat ./static-dicomweb.json5
 
 # Start the first process
 dicomwebserver &
-
-echo Started dicomwebserver
 
 # Start the second process
 dicomwebscp
