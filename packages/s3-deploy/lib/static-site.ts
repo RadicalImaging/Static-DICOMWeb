15eae078b6b9441b7448be43648a4b76a5a1cc2a
Pedro Kohler <pedrokohlerbh@gmail.com>
2024-05-21 16:11:16 -0300

    fix: remove unused variable breaking build

--- packages/s3-deploy/lib/static-site.ts
+++ packages/s3-deploy/lib/static-site.ts
@@ -26,19 +26,10 @@ export class StaticSite extends Construct {
     const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, `${name}-OAI`, {
       comment: `OAI for ${name}`
     });
-    const oac = new cloudfront.CfnOriginAccessControl(this, 'StaticDICOMweb-OAC',{
-      originAccessControlConfig: {
-        name: 'StaticDICOMweb-OAC-Name',
-        originAccessControlOriginType: 's3',
-        signingBehavior: 'always',
-        signingProtocol: 'sigv4',
-        description: 'Generic origin access control for Static DICOMweb files in s3',
-      },
-    });
 
     // Ordered, so get first value for primary, and then everything else
     const distProps = new Map();
-    
+
     if (props.clientGroup) {
       const clientGroup = configGroup(props,"client");
       console.log("clientGroup:", clientGroup);
@@ -46,11 +37,11 @@ export class StaticSite extends Construct {
     } else {
       console.log("no clientGroup specified for deployment:", name);
     }
-    
+
     if( props.indexGroup ) {
       const group = configGroup(props,"index");
       console.log("Index group", group);
-      createDocumentGroup(this,group);  
+      createDocumentGroup(this,group);
     }
 
     if (props.rootGroup) {
@@ -73,7 +64,7 @@ export class StaticSite extends Construct {
     const distIterator = distProps.entries();
     const [ , defaultDistProps] = distIterator.next().value;
     const additionalDistProps = distProps.size>1 ? Object.fromEntries(distIterator) : undefined;
-    
+
     const siteInfo = getSiteInfo(this, name, props);
     const siteDomain = siteInfo?[siteInfo.siteDomain]:undefined;
 
@@ -87,14 +78,14 @@ export class StaticSite extends Construct {
       httpVersion:  cloudfront.HttpVersion.HTTP3,
     });
 
-  
+
     new CfnOutput(this, 'DistributionId', { value: distribution.distributionId });
 
     if (siteInfo) {
       configureDomain(this, name, siteInfo.siteDomain, siteInfo.zone, distribution);
     }
     new CfnOutput(this, 'DistributionDomainName', { value: siteInfo?siteInfo.siteDomain:distribution.distributionDomainName });
-    
+
     // Deploy site contents to S3 bucket
     const clientDir = handleHomeRelative(props.clientDir || './site-contents');
     new CfnOutput(this, 'clientDir', { value: clientDir } );
