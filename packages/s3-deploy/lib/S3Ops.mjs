d180414715efedb76348a4c81291467520877398
Bill Wallace <wayfarer3130@gmail.com>
2024-06-07 17:44:30 -0400

    Fix/add info option (#42)
    * Fix a deployment denied issue
    
    * Husky check

--- packages/s3-deploy/lib/S3Ops.mjs
+++ packages/s3-deploy/lib/S3Ops.mjs
@@ -150,7 +150,7 @@ class S3Ops {
     for (let i = 1; i < ETag.length - 1; i++) {
       if (md5[i] != ETag.charCodeAt(i)) {
         // Leave this for now as there might be more file types needing specific encoding checks
-        console.log("md5 different at", i, md5[i], ETag.charCodeAt(i), ETag, md5.toString());
+        console.warn("md5 different at", i, md5[i], ETag.charCodeAt(i), ETag, md5.toString());
         return false;
       }
     }
@@ -169,7 +169,7 @@ class S3Ops {
       Key,
     });
     if (options?.force !== true && fs.existsSync(destFile)) {
-      console.verbose("Skipping", destFile);
+      console.info("Already exists", Key);
       return destFile;
     }
 
@@ -182,9 +182,9 @@ class S3Ops {
       const result = await this.client.send(command);
       const { Body } = result;
       await copyTo(Body, destFile);
-      console.verbose("Done copyTo destDir");
+      console.info("Retrieved", Key);
     } catch (e) {
-      console.log("Error retrieving", Bucket, Key, e);
+      console.warn("Error retrieving", Bucket, Key, e);
     }
 
     return destFile;
@@ -254,6 +254,8 @@ class S3Ops {
    * Asynchronous
    */
   async upload(dir, file, hash, ContentSize, excludeExisting = {}) {
+    // Exclude the Mac garbage
+    if (file && file.indexOf(".DS_STORE") !== -1) return false;
     const Key = this.fileToKey(file);
     const ContentType = this.fileToContentType(file);
     const Metadata = this.fileToMetadata(file, hash);
@@ -279,8 +281,9 @@ class S3Ops {
       ContentSize,
     });
     console.verbose("uploading", file, ContentType, ContentEncoding, Key, ContentSize, Metadata, this.group.Bucket);
+    console.info("Stored", Key);
     if (this.options.dryRun) {
-      console.log("Dry run - no upload", Key);
+      console.log("Dry run - not stored", Key);
       // Pretend this uploaded - causes count to change
       return true;
     }
