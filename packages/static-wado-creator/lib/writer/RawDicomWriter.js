3fe70d19b9d5c6b13320c5ff947c0f80dc602f74
Bill Wallace <wayfarer3130@gmail.com>
2024-11-04 12:57:44 -0500

    fix: Add raw write for parametric sop class (#70)
    * fix: Add raw write for parametric sop class
    
    * fix: formatting to match OHIF
    
    * Add formatting

--- packages/static-wado-creator/lib/writer/RawDicomWriter.js
+++ packages/static-wado-creator/lib/writer/RawDicomWriter.js
@@ -4,10 +4,15 @@ const WriteMultipart = require("./WriteMultipart");
 const { MultipartHeader } = require("./MultipartHeader");
 
 const modalitiesToRawWrite = ["SEG", "SR"];
+const sopClassesToRawWrite = ["1.2.840.10008.5.1.4.1.1.30"];
 
 const defaultSelector = (instance) => {
   const modality = instance.metadata[Tags.Modality]?.Value?.[0];
-  return modalitiesToRawWrite.indexOf(modality) !== -1;
+  const sopClass = instance.metadata[Tags.SOPClassUID]?.Value?.[0];
+  return (
+    modalitiesToRawWrite.indexOf(modality) !== -1 ||
+    sopClassesToRawWrite.indexOf(sopClass) !== -1
+  );
 };
 
 /** Writes out raw DICOM files, encapsulated in multipart and gzipped - currently just DICOM SR and SEG */
@@ -28,7 +33,11 @@ const RawDicomWriter =
     });
     const { length } = content;
     const buffer = content.slice(0, length);
-    await WriteMultipart(writeStream, [new MultipartHeader("Content-Type", contentType, [])], buffer);
+    await WriteMultipart(
+      writeStream,
+      [new MultipartHeader("Content-Type", contentType, [])],
+      buffer
+    );
     await writeStream.close();
   };
 RawDicomWriter.defaultSelector = defaultSelector;
