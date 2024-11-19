4a8b86dbace3fab19514f1e709c7dc7acd3c60e6
Bill Wallace <wayfarer3130@gmail.com>
2024-11-19 07:19:47 -0500

    fix: dockerfile updates and runnable docker compose (#71)
    * fix: Changes to get dockerfile working again
    
    * Update the ignore info
    
    * fix: Build dockerfile correctly and update versions
    
    * fix: Make the docker build faster

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,24 @@
-FROM node:18
+# syntax=docker/dockerfile:1.7-labs
+FROM node:20 as builder
 WORKDIR /app
-RUN git clone https://github.com/RadicalImaging/Static-DICOMWeb.git /app
-RUN git fetch
-RUN git checkout feat/docker
-RUN git pull
-RUN yarn install
+RUN yarn config set workspaces-experimental true
+ENV PATH /app/node_modules/.bin:$PATH
 RUN npm install -g lerna@5.3.0
 RUN npm install typescript
-RUN yarn build
+COPY --parents yarn.lock package.json packages/*/package.json .
+RUN --mount=type=cache,target=/usr/local/share/.cache/yarn \
+    yarn install --ignore-scripts --production 
+COPY --link --exclude=node_modules --exclude=**/build . .
+RUN yarn run build
 RUN yarn link:exec
 RUN mkdir /dicomweb
 RUN mkdir ~/.aws
+
+
+RUN yarn build
+RUN yarn link:exec
 COPY ./docker/* .
 RUN chmod ugo+x ./startStaticDicomweb.sh
 EXPOSE 5000
-EXPOSE 11112
+EXPOSE 11115
 ENTRYPOINT ["/bin/sh", "-c" , "/app/startStaticDicomweb.sh"]
---
+++ eslint.config.mjs
@@ -0,0 +1,53 @@
+import jest from "eslint-plugin-jest";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: [
+      "**/.eslintrc.js",
+      "**/dist",
+      "**/build",
+      "**/node_modules",
+      "**/rollup.config.js",
+      "**/jest.config.js",
+    ],
+  },
+  {
+    plugins: {
+      jest,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+        ...globals.node,
+        ...globals.commonjs,
+        ...jest.environments.globals.globals,
+      },
+
+      ecmaVersion: 2022,
+      sourceType: "commonjs",
+    },
+
+    rules: {
+      curly: "warn",
+      "node/no-unsupported-features/es-syntax": 0,
+      semi: "warn",
+    },
+  },
+  // Any other config imports go at the top
+  eslintPluginPrettierRecommended,
+];
