a765ac27c026dfdbfa83e184c84c336e9de62202
Bill Wallace <wayfarer3130@gmail.com>
2025-03-03 17:00:35 -0500

    fix: stow-rs response

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # syntax=docker/dockerfile:1.7-labs
-FROM node:20 as builder
+FROM node:22 as builder
 WORKDIR /app
 RUN yarn config set workspaces-experimental true
 ENV PATH /app/node_modules/.bin:$PATH
@@ -10,15 +10,18 @@ RUN --mount=type=cache,target=/usr/local/share/.cache/yarn \
     yarn install --ignore-scripts --production 
 COPY --link --exclude=node_modules --exclude=**/build . .
 RUN yarn run build
-RUN yarn link:exec
+
+FROM node:22 as dicomwebserver
+WORKDIR /app
+RUN npm install -g commander@10.0.1
+COPY --from=builder /app/packages/static-wado-util/*.tgz static-wado-util.tgz
+COPY --from=builder /app/packages/static-cs-lite/*.tgz static-cs-lite.tgz
+COPY --from=builder /app/packages/static-wado-creator/*.tgz static-wado-creator.tgz
+RUN npm install -g ./static-wado-util.tgz ./static-cs-lite.tgz 
+RUN npm install -g ./static-wado-creator.tgz
 RUN mkdir /dicomweb
 RUN mkdir ~/.aws
-
-
-RUN yarn build
-RUN yarn link:exec
 COPY ./docker/* .
-RUN chmod ugo+x ./startStaticDicomweb.sh
 EXPOSE 5000
 EXPOSE 11115
-ENTRYPOINT ["/bin/sh", "-c" , "/app/startStaticDicomweb.sh"]
+# CMD ["mkdicomweb", "--help"]
--- packages/static-wado-webserver/lib/controllers/server/commonControllers.mjs
+++ packages/static-wado-webserver/lib/controllers/server/commonControllers.mjs
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
-import formidable from "formidable";
-import * as storeServices from "../../services/storeServices.mjs";
+import formidable from "formidable"
+import * as storeServices from "../../services/storeServices.mjs"
 
 /**
  * Handles an incoming stow-rs POST data, either in application/dicom (single instance), or in
@@ -13,21 +13,34 @@ import * as storeServices from "../../services/storeServices.mjs";
  */
 export function defaultPostController(params) {
   return (req, res, next) => {
-    const form = formidable({ multiples: true });
+    const files = []
+
+    const form = formidable({ multiples: true })
+    form.on("file", (_formname, file) => {
+      const { filepath, mimetype } = file
+      files.push({
+        filepath,
+        mimetype,
+      })
+    })
+
     form.parse(req, async (err, fields, files) => {
       if (err) {
-        console.log("Couldn't parse because", err);
-        next(err);
-        return;
+        console.log("Couldn't parse because", err)
+        next(err)
+        return
       }
       try {
-        const sopInfo = await storeServices.storeFilesByStow(files, params);
-        console.log("Returning empty result - TODO, generate references", sopInfo);
-        res.status(200).json({});
+        const sopInfo = await storeServices.storeFilesByStow(files, params)
+        console.log(
+          "Returning empty result - TODO, generate references",
+          sopInfo
+        )
+        res.status(200).json({})
       } catch (e) {
-        console.log(e);
-        res.status(500).text(`Unable to handle ${e}`);
+        console.log(e)
+        res.status(500).text(`Unable to handle ${e}`)
       }
-    });
-  };
+    })
+  }
 }
