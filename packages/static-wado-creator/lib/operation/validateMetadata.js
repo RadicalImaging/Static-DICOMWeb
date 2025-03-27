888339fe40b6904475484ee18092f4b8269c20d6
Bill Wallace <wayfarer3130@gmail.com>
2025-03-27 13:40:37 -0400

    fix: Change output format to include multipart surround

--- packages/static-wado-creator/lib/operation/validateMetadata.js
+++ packages/static-wado-creator/lib/operation/validateMetadata.js
@@ -1,14 +1,36 @@
+const { Tags } = require("@radicalimaging/static-wado-util")
+
 function validateMetadata(options) {
-  // console.log("validateMetadata options", options)
-  return function (id, result, buffer) {
-    console.verbose("Validating metadata", id, result, buffer)
+  console.verbose("validateMetadata options", options)
+  const serviceUrl = ""
+
+  return function (id, result) {
+    const getValue = Tags.getValue.bind(null, result.metadata)
+    console.verbose("Validating metadata", id, result)
     if (id.studyInstanceUid.length > 64) {
       throw new Error(`StudyInstanceUID too long: ${id.studyInstanceUid}`)
     }
     if (id.seriesInstanceUid.length > 64) {
       throw new Error(`SeriesInstanceUID too long: ${id.studyInstanceUid}`)
     }
-    // TODO - add check here
+    const RetrieveURL = `${serviceUrl}${id.studyInstanceUid}/series/${id.seriesInstanceUid}/instances/${id.sopInstanceUid}`
+
+    // TODO - add check here from plugin to add remote query checks, eg kheops or similar
+    const successMessage = {
+      ReferencedSOPSequence: [
+        {
+          ReferencedSOPClassUID: getValue(Tags.SOPClassUID),
+          ReferencedSOPInstanceUID: id.sopInstanceUid,
+          RetrieveURL,
+        },
+      ],
+    }
+    const responseMessage = JSON.stringify(successMessage)
+    console.log(
+      "--boundary-response\r\n\r\n" +
+        responseMessage +
+        "\r\n--boundary-response--\r\n"
+    )
   }
 }
 
