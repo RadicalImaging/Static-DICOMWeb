cffd5b77a949de5762778bb1c1475d260a2e2a6c
Thomas Forster <thomas.forster@radicalimaging.com>
2025-05-22 10:06:00 -0300

    Changed hash from 4 digits to 3

--- packages/static-wado-util/lib/getStudyUIDPathAndSubPath.js
+++ packages/static-wado-util/lib/getStudyUIDPathAndSubPath.js
@@ -1,9 +1,9 @@
 import MurmurHash3 from "imurmurhash";
 
 export default function getStudyUIDPathAndSubPath(studyUID) {
-  const hash = new MurmurHash3(studyUID).result().toString(16).padStart(8, "0");
-  const path = hash.substring(0, 4);
-  const subpath = hash.substring(4, 8);
+  const hash = new MurmurHash3(studyUID).result().toString(16).padStart(6, "0");
+  const path = hash.substring(0, 3);
+  const subpath = hash.substring(3, 6);
 
   return { path, subpath };
 }
